687da97372e1749eaf098009d8a097578a80c199
markokajzer <markokajzer91@gmail.com>
2016-06-06 18:44:13 +0200

    Added primitive caching system

--- controllers/redirect_controller.rb
+++ controllers/redirect_controller.rb
@@ -1,24 +1,44 @@
 class RedirectController < ApplicationController
 
-  get '/' do
-    videos = []
-    max_results = count = 0
-    next_page = ''
+  cache = {}
 
+  get '/' do
     playlist = params['playlist'] || PLAYLIST_ID
 
-    while not count > max_results
-      response = JSON.parse(open("#{REQUEST_BASE}&playlistId=#{playlist}&pageToken=#{next_page}").read)
-      response['items'].each do |video|
-        videos << video['contentDetails']['videoId']
+    videos = if cache[playlist].present?
+      if cache[playlist]['cache_until'] < Time.now
+        save_video_ids(playlist, cache)
+      else
+        cache[playlist]['videos']
       end
-
-      count += response['pageInfo']['resultsPerPage']
-      max_results = response['pageInfo']['totalResults'] if max_results.zero?
-      next_page = response['nextPageToken']
+    else
+      save_video_ids(playlist, cache)
     end
 
     redirect to("#{YOUTUBE_BASE}#{videos.sample}&list=#{playlist}")
+  end
+
+  helpers do
+    def save_video_ids(playlist, cache)
+      videos = []
+      max_results = count = 0
+      next_page = ''
+
+      while not count > max_results
+        response = JSON.parse(open("#{REQUEST_BASE}&playlistId=#{playlist}&pageToken=#{next_page}").read)
+        response['items'].each do |video|
+          videos << video['contentDetails']['videoId']
+        end
+
+        count += response['pageInfo']['resultsPerPage']
+        max_results = response['pageInfo']['totalResults'] if max_results.zero?
+        next_page = response['nextPageToken']
+      end
+
+      cache[playlist] = {}
+      cache[playlist]['cache_until'] = Time.now + 24.hours
+      cache[playlist]['videos'] = videos
+    end
   end
 
 end
